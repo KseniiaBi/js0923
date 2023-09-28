b41549a5831cfebc5962c6eaa555ee7169009083
Memoqer <108836710+Memoqer@users.noreply.github.com>
2023-09-28 19:12:35 +0300

    Homework

---
+++ task.js
@@ -0,0 +1,42 @@
+// let numbers = 15;
+
+// if (numbers >= 0 && numbers <= 14) {
+//     console.log("Число в первой четверти часа");
+// } else if (numbers >= 15 && numbers <= 29) {
+//     console.log("Число во второй четверти часа");
+// } else if (numbers >= 30 && numbers <= 44) {
+//     console.log("Число в третьей четверти часа");
+// } else {
+//     console.log("Число попадает в четвертую четверть часа");
+// }
+
+
+
+
+let a = 5; 
+
+if (a === 0 || a === 2) {
+    a += 7;
+} else {
+    a = a/10;
+}
+
+
+console.log(a);
+
+
+
+
+let test = true; 
+
+if (test === true) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+
+
+
+
+
+
