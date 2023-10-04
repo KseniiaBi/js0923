5950e40a58cb087758fa3b602ecc7bf20cbc9a87
Memoqer <108836710+Memoqer@users.noreply.github.com>
2023-10-04 23:59:57 +0300

    Hometask3

--- task.js
+++ task.js
@@ -13,28 +13,62 @@
 
 
 
-let a = 5; 
+// let a = 5; 
 
-if (a === 0 || a === 2) {
-    a += 7;
-} else {
-    a = a/10;
-}
-
-
-console.log(a);
+// if (a === 0 || a === 2) {
+//     a += 7;
+// } else {
+//     a = a/10;
+// }
 
 
+// console.log(a);
 
 
-let test = true; 
 
-if (test === true) {
-    console.log('Верно');
-} else {
-    console.log('Неверно');
-}
 
+// let test = true; 
+
+// if (test === true) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
+
+// 1) создать два массива с длиной 10 и произвольными числами в нем. Из них слепить третий, в котором каждый элемент будет суммой элементов с тем же индексом из двух базовых массивов.
+
+// const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const array2 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+// const resultArray = [];
+
+
+// for (let i = 0; i < array1.length; i++) {
+//   resultArray[i] = array1[i] + array2[i];
+// }
+
+
+// console.log(resultArray);
+
+
+// Найти сумму чисел в массиве [[1,2,3], [4,5,6]]
+
+
+// const Array = [[1, 2, 3], [4, 5, 6]];
+
+
+// let sum = 0;
+
+
+// for (let i = 0; i < Array.length; i++) {
+//   for (let j = 0; j < Array[i].length; j++) {
+  
+//     sum += Array[i][j];
+//   }
+// }
+
+
+// console.log(sum);
 
 
 
